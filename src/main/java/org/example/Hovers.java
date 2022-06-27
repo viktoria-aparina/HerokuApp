a2c2b576c8125a2f53c03f960d80d91156121599
viktoria-aparina <victoria.aparina.s@yandex.by>
2022-06-27 15:53:44 +0300

    feature-6-hovers: fixed Hovers

--- src/main/java/org/example/Hovers.java
+++ src/main/java/org/example/Hovers.java
@@ -3,26 +3,26 @@ package org.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
-public class Hovers extends BaseTest { //на втором элементе валится, не может найти имя, не знаю почему!?
+public class Hovers extends BaseTest {
     @Test
     public void hovers() {
         driver.findElement(By.linkText("Hovers")).click();
-        List<WebElement> usersPhotos = driver.findElements(By.cssSelector("img[alt='User Avatar']")); //src='/img/avatar-blank.jpg'
+        List<WebElement> usersPhotos = driver.findElements(By.cssSelector(".figure"));
+        Actions actions = new Actions(driver);
         for (WebElement element : usersPhotos) {
-            Actions actions = new Actions(driver);
             actions.moveToElement(element).build().perform();
-            WebElement name = driver.findElement(By.xpath("//*[contains(text(), 'name:')]"));
-            Assert.assertTrue(name.isDisplayed());
-            WebElement viewProfile = driver.findElement(By.xpath("//a[contains(@href,'users')]"));
-            actions.moveToElement(viewProfile).click(viewProfile).build().perform();
-            WebElement mistake = driver.findElement(By.xpath("/html/body/h1"));
-            Assert.assertTrue(mistake.isDisplayed());
+            element.findElement(By.tagName("h5")).isDisplayed();
+            element.findElement(By.linkText("View profile")).click();
+            driver.findElement(By.tagName("h1")).isDisplayed();
             driver.navigate().back();
         }
     }
 }
+
+
+
+
